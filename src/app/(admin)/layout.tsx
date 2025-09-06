dd862dbac9a75d30471edcf1110dada3d9897aa7
shadil-rayyan <shadilrayyan2@gmail.com>
2025-09-06 19:51:23 +0530

    added the admin route checking program

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -7,7 +7,7 @@ import Sidebar from "@/components/admin/SideBar";
 import { doc, getDoc } from 'firebase/firestore';
 import { auth, firestore } from '@/lib/firebase/config'
 import TopBar from '@/components/admin/TopBar';
-
+import loading from '@/app/loading';
 interface AdminLayoutProps {
   children: ReactNode;
 }
@@ -20,39 +20,39 @@ const AdminLayout: React.FC<AdminLayoutProps> = ({ children }) => {
   const router = useRouter();
 
   // Check user authentication and admin role on mount
-  // useEffect(() => {
-  //   const checkAuth = async () => {
-  //     const unsubscribe = onAuthStateChanged(async (authUser) => {
-  //       if (authUser) {
-  //         // Check if the authenticated user is an admin
-  //         const userDocRef = doc(firestore, 'adminemail', authUser.email as string);
-  //         const userDoc = await getDoc(userDocRef);
-  //         const isAdmin = userDoc.exists() && userDoc.data()?.role === 'admin';
+  useEffect(() => {
+    const checkAuth = async () => {
+      const unsubscribe = onAuthStateChanged(async (authUser) => {
+        if (authUser) {
+          // Check if the authenticated user is an admin
+          const userDocRef = doc(firestore, 'adminemail', authUser.email as string);
+          const userDoc = await getDoc(userDocRef);
+          const isAdmin = userDoc.exists() && userDoc.data()?.role === 'admin';
 
-  //         if (isAdmin) {
-  //           setIsAuthenticated(true); // Set the state to authenticated if user is admin
-  //         } else {
-  //           setIsAuthenticated(false);
-  //           router.push('/auth/Login'); // Redirect to login if not an admin
-  //         }
-  //       } else {
-  //         setIsAuthenticated(false); // If no authenticated user, redirect to login
-  //         router.push('/auth/Login');
-  //       }
-  //       // setLoading(false); // Stop loading after check
-  //     });
+          if (isAdmin) {
+            setIsAuthenticated(true); // Set the state to authenticated if user is admin
+          } else {
+            setIsAuthenticated(false);
+            router.push('/auth/Login'); // Redirect to login if not an admin
+          }
+        } else {
+          setIsAuthenticated(false); // If no authenticated user, redirect to login
+          router.push('/auth/Login');
+        }
+        // setLoading(false); // Stop loading after check
+      });
 
-  //     // Cleanup subscription on unmount
-  //     return () => unsubscribe();
-  //   };
+      // Cleanup subscription on unmount
+      return () => unsubscribe();
+    };
 
-  //   checkAuth();
-  // }, [router]);
+    checkAuth();
+  }, [router]);
 
   // Show a loading indicator while checking authentication
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="flex h-screen bg-gray-100 overflow-hidden">
